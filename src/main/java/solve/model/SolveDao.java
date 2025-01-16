89df32bdbf6cc5adaa0e354b385887ecd3d914f2
zkdn46 <123gusckd123@naver.com>
2025-01-16 17:51:10 +0900

    Add SolveDao.java

--- src/main/java/solve/model/SolveDao.java
+++ src/main/java/solve/model/SolveDao.java
@@ -1,5 +1,63 @@
 package solve.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import util.DBManager;
+
 public class SolveDao {
+
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	private SolveDao() {
+
+	}
+
+	private static SolveDao instance = new SolveDao();
+
+	public static SolveDao getInstance() {
+		return instance;
+	}
+
+	public ArrayList<Solve> findMySolveAll(String userCode) {
+		ArrayList<Solve> list = new ArrayList<>();
+
+		conn = DBManager.getConnection();
+
+		if (conn != null) {
+			String sql = "SELECT * FROM solve WHERE=?";
+
+			try {
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, userCode);
+				rs = pstmt.executeQuery();
+
+				while (rs.next()) {
+					int code = rs.getInt(1);
+					int quizCode = rs.getInt(3);
+					int score = rs.getInt(4);
+					int timer = rs.getInt(5);
+					Timestamp regDate = rs.getTimestamp(6);
+					Timestamp modDate = rs.getTimestamp(7);
+
+					Solve solve = new Solve(code, userCode, quizCode, score, timer, regDate, modDate);
+
+					list.add(solve);
+				}
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				DBManager.close(conn, pstmt, rs);
+			}
+		}
+		return list;
+	}
 
 }
