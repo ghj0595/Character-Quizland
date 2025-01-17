f2bc06120bf82dca2e69529a4a9ca883337a8b10
zkdn46 <123gusckd123@naver.com>
2025-01-17 17:05:11 +0900

    Modify listCount, totalScore

--- src/main/java/user/action/ViewAction.java
+++ src/main/java/user/action/ViewAction.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import controller.Action;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -24,22 +25,20 @@ public class ViewAction implements Action {
         }
 
         SolveDao solveDao = SolveDao.getInstance();
-        ArrayList<Solve> solvelist = solveDao.findMySolveAll(user.getUserCode());
 
-        int listCount = solvelist.size();
+        
+        int listCount = solveDao.getSizeByUser(user.getUserCode());
 
-        int totalScore = 0;
-        for (Solve solve : solvelist) {
-            totalScore += solve.getScore();
-        }
+        int totalScore = solveDao.getTotalScoreByUser(user.getUserCode());
 
         double avgScore = (listCount > 0) ? (double) totalScore / listCount : 0;
-
-        session.setAttribute("total-game", listCount);
-        session.setAttribute("total-score", totalScore);
-        session.setAttribute("avg-score", avgScore);
+        
+        session.setAttribute("totalGame", listCount);
+        session.setAttribute("totalScore", totalScore);
+        session.setAttribute("avgScore", avgScore);
         session.setAttribute("log", user);
-
-        response.sendRedirect("/myPage");
+        
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/mypage");
+        dispatcher.forward(request, response);
     }
 }
