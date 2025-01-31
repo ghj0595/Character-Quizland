de5874afba1d6a88a3d440da447ed6c12e8004ec
ghj0595 <ghj0595@naver.com>
2025-01-31 14:20:25 +0900

    Modify QUiz List

--- src/main/java/admin/action/QuizListAction.java
+++ src/main/java/admin/action/QuizListAction.java
@@ -16,11 +16,20 @@ public class QuizListAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		SolveDao solveDao = SolveDao.getInstance(); 
-		JSONArray quizSolveData = solveDao.findQuizSolveAll(1, 10); 
-		request.setAttribute("quizList", quizSolveData.toList()); 
-		request.getRequestDispatcher("/quizzes").forward(request, response);
-	}
+		int page = 1;
+        int itemsPerPage = 10;
+        
+        if (request.getParameter("page") != null) {
+            page = Integer.parseInt(request.getParameter("page"));
+        }
+
+        SolveDao solveDao = SolveDao.getInstance(); 
+        JSONArray quizSolveData = solveDao.findQuizSolveAll(page, itemsPerPage); 
+        request.setAttribute("quizList", quizSolveData.toList()); 
+        request.setAttribute("currentPage", page);
+        request.setAttribute("totalPages", (int) Math.ceil(solveDao.getTotalSize() / (double) itemsPerPage));
+        request.getRequestDispatcher("/quizzes").forward(request, response);
+    }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
